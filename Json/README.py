31ac54373b685c39a80bd5ba45b681474f372bf9
Neha <saxenaneha2110@gmail.com>
2026-06-09 12:06:41 +0530

    Update README.py

--- Json/README.py
+++ Json/README.py
@@ -1,1 +1,18 @@
 
+# JSON Parsing Examples
+
+This folder contains examples of reading and parsing JSON data using Python.
+
+## Topics Covered
+
+- Reading JSON files
+- Accessing JSON keys
+- Working with dictionaries
+- Preparing for REST API responses
+
+## Scripts
+
+| Script | Purpose |
+|----------|----------|
+| parse_device_info.py | Read device details from JSON |
+| parse_interfaces.py | Read interface information |
