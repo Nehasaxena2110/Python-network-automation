f73d942f1997205b2509e0c1dc4b6ea1be28835c
Neha <saxenaneha2110@gmail.com>
2026-06-09 11:13:24 +0530

    Update show_version.py

--- Netmiko/show_version.py
+++ Netmiko/show_version.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 
+# Device details
 device = {
     "device_type": "cisco_ios",
     "host": "10.1.1.1",
@@ -7,10 +8,14 @@ device = {
     "password": "YOUR_PASSWORD"
 }
 
+# Connect to device
 connection = ConnectHandler(**device)
 
+# Run command
 output = connection.send_command("show version")
 
+# Display output
 print(output)
 
+# Close connection
 connection.disconnect()
